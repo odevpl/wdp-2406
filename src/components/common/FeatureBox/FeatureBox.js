6153d156158e996f0dfef69705e27712a177a8ca
AS <arssoo569@gmail.com>
2024-05-17 23:32:06 +0200

    implementing useState to manage hover state for FeatureBox component

--- src/components/common/FeatureBox/FeatureBox.js
+++ src/components/common/FeatureBox/FeatureBox.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import styles from './FeatureBox.module.scss';
+import { useState } from 'react';
 
-const FeatureBox = ({ active, icon, children }) => (
-  <div className={styles.root + (active ? ' ' + styles.active : '')}>
-    {icon && (
-      <div className={styles.iconWrapper}>
-        <FontAwesomeIcon className={styles.icon} icon={icon} />
-      </div>
-    )}
-    <div className={styles.content}>{children}</div>
-  </div>
-);
+const FeatureBox = ({ active, icon, children }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      className={styles.root + (isHovered ? ' ' + styles.active : '')}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {icon && (
+        <div className={styles.iconWrapper}>
+          <FontAwesomeIcon className={styles.icon} icon={icon} />
+        </div>
+      )}
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
+};
 
 FeatureBox.propTypes = {
   children: PropTypes.node,
