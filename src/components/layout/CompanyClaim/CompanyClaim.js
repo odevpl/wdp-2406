36ef09f6131d749d8f5e0951e1be36b5990719cf
AS <arssoo569@gmail.com>
2024-05-24 00:19:13 +0200

    modifying CompanyClaim component

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -10,18 +10,22 @@ const CompanyClaim = () => (
   <div className={styles.root}>
     <div className='container'>
       <div className='row align-items-center'>
-        <div className={`col text-left ${styles.phoneNumber}`}>
+        <div
+          className={`col-12 col-md text-left order-3 order-md-1 ${styles.phoneNumber}`}
+        >
           <p>
             <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
             222
           </p>
         </div>
-        <div className='col text-center'>
+        <div className='col text-left text-md-center order-1 order-md-2'>
           <a href='#'>
             <img src='/images/logo.png' alt='Bazar' />
           </a>
         </div>
-        <div className={`col text-right ${styles.cart}`}>
+        <div
+          className={`col text-right order-2 order-md-3 align-self-start align-self-md-center mt-2 mt-md-0 ${styles.cart}`}
+        >
           <a href='#' className={styles.cartBox}>
             <div className={styles.cartIcon}>
               <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
