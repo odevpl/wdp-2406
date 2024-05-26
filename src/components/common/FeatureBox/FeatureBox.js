48635a1fb10e8d5789f1157b6ec8561bbc764be5
AS <arssoo569@gmail.com>
2024-05-26 23:22:37 +0200

    removing tests and changing concept to anchor wrapper

--- src/components/common/FeatureBox/FeatureBox.js
+++ src/components/common/FeatureBox/FeatureBox.js
@@ -4,24 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './FeatureBox.module.scss';
 import { useState } from 'react';
 
-const FeatureBox = ({ icon, target, children }) => {
+const FeatureBox = ({ icon, children }) => {
   const [isHovered, setIsHovered] = useState(false);
-
-  const scrollToAnchor = targetId => {
-    var element = document.getElementById(targetId);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
-  };
-
-  if (!target) target = '';
 
   return (
     <div
       className={styles.root + (isHovered ? ' ' + styles.active : '')}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      onClick={() => scrollToAnchor(target)}
     >
       {icon && (
         <div className={styles.iconWrapper}>
@@ -36,7 +26,6 @@ const FeatureBox = ({ icon, target, children }) => {
 FeatureBox.propTypes = {
   children: PropTypes.node,
   icon: PropTypes.object,
-  target: PropTypes.string,
 };
 
 export default FeatureBox;
