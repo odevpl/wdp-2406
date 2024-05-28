46b95a1caf3a755f25777de982ba4b85a69d1e73
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-05-28 23:33:19 +0200

    WDP240601-14 Fix mobile layout

--- src/components/layout/MenuBar/MenuBar.js
+++ src/components/layout/MenuBar/MenuBar.js
@@ -33,10 +33,6 @@ const MenuBar = ({ children }) => {
         className={name === selectedCategory ? styles.active : ''}
       >
         <a href='#' key={name}>
-          <div className={styles.hamburger}>
-            <FontAwesomeIcon icon={faBars} />{' '}
-          </div>
-
           {name}
         </a>
       </li>
@@ -46,15 +42,35 @@ const MenuBar = ({ children }) => {
     <div className={clsx(styles.root)}>
       <div className={styles.container}>
         <div className='row align-items-center justify-content-center m-0'>
-          <div className={clsx('col-12', 'col-lg-5', 'order-2', 'order-lg-1')}>
+          <div
+            className={clsx(
+              'col-10',
+              'col-sm-12',
+              'col-lg-5',
+              'order-1',
+              'order-sm-2',
+              'order-lg-1'
+            )}
+          >
             <ProductSearch />
           </div>
-
+          <div className={clsx('d-sm-none', 'col-2', 'order-2')}>
+            <button
+              className={styles.menuToggler}
+              onClick={() => {
+                setIsExpanded(!isExpanded);
+              }}
+            >
+              <FontAwesomeIcon icon={faBars} className={styles.hamburger} />
+            </button>
+          </div>
           <div
             className={clsx(
               'col-12',
+              'col-sm-12',
               'col-lg-7',
-              'order-1',
+              'order-3',
+              'order-sm-1',
               'order-lg-2',
               'p-0',
               'px-sm-4',
