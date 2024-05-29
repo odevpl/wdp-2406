7f0e8fac5e3b7001c1a79db7848572d9459f6f07
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-05-30 00:39:34 +0200

    WDP240601-25 Promotions done with RWD for WDP240601-26

--- src/components/features/Promotions/Promotions.js
+++ src/components/features/Promotions/Promotions.js
@@ -8,8 +8,8 @@ const Promotions = () => (
   <div className={styles.root}>
     <div className='container'>
       <div className={clsx('row', styles.box)}>
-        <div className='col-6 '>
-          <div className={clsx(styles.promotionPrimary)}>
+        <div className='col-12 col-md-6 my-2 my-md-0'>
+          <div className={clsx(styles.promotion, styles.promotionPrimary)}>
             <img
               src={`${process.env.PUBLIC_URL + '/images/sofas/sofa-2.webp'}`}
               alt='bed'
@@ -24,9 +24,55 @@ const Promotions = () => (
             </div>
           </div>
         </div>
-        <div className='col-6 '>
-          <div className={clsx('row', styles.promotionSecondary)}></div>
-          <div className={clsx('row', styles.promotionSecondary)}></div>
+        <div className='col-12 col-md-6'>
+          <div className='row'>
+            <div
+              className={clsx(
+                'col-12',
+                'my-2',
+                'my-md-0',
+                'mb-md-2',
+                styles.promotion,
+                styles.promotionSecondary
+              )}
+            >
+              <img
+                src={`${process.env.PUBLIC_URL + '/images/sofas/sofa-2.webp'}`}
+                alt='bed'
+              />
+
+              <div className={styles.shadow}></div>
+              <div className={styles.message}>
+                <h3>
+                  <b>Office</b> Chair
+                </h3>
+                <h6>Collection</h6>
+                <h2>$200.00</h2>
+              </div>
+            </div>
+            <div
+              className={clsx(
+                'col-12',
+                'my-2',
+                'my-md-0',
+                'mt-md-2',
+                styles.promotion,
+                styles.promotionTertiary
+              )}
+            >
+              <img
+                src={`${process.env.PUBLIC_URL + '/images/sofas/sofa-2.webp'}`}
+                alt='bed'
+              />
+
+              <div className={styles.message}>
+                <h5>
+                  <b>Special</b> Collection
+                </h5>
+                <p>Save up 45% of furniture</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
