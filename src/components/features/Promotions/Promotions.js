a6a96083b4715dd3e3d646d9e825f00903c5df68
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-05-29 22:41:38 +0200

    WDP240601-25 First promotional message

--- src/components/features/Promotions/Promotions.js
+++ src/components/features/Promotions/Promotions.js
@@ -8,7 +8,22 @@ const Promotions = () => (
   <div className={styles.root}>
     <div className='container'>
       <div className={clsx('row', styles.box)}>
-        <div className='col-6 '></div>
+        <div className='col-6 '>
+          <div className={clsx(styles.promotionPrimary)}>
+            <img
+              src={`${process.env.PUBLIC_URL + '/images/sofas/sofa-2.webp'}`}
+              alt='bed'
+            />
+            <div className={styles.shadow}></div>
+            <div className={styles.message}>
+              <h2>Guest room</h2>
+              <h1>Sofa</h1>
+              <div className={styles.box}>
+                <h2>-20%</h2>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className='col-6 '>
           <div className={clsx('row', styles.promotionSecondary)}></div>
           <div className={clsx('row', styles.promotionSecondary)}></div>
