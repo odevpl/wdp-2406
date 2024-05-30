30ae06cc159ee7aadfe42518b30c3107d482b283
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-05-30 23:55:00 +0200

    WDP240601-27 ProductBox2 Layout done

--- src/components/common/ProductBox2/ProductBox2.js
+++ src/components/common/ProductBox2/ProductBox2.js
@@ -4,18 +4,23 @@ import PropTypes from 'prop-types';
 
 import styles from './ProductBox2.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faExchangeAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
-import { faHeart } from '@fortawesome/free-regular-svg-icons';
-import Button from '../Button/Button';
+import {
+  faExchangeAlt,
+  faShoppingBasket,
+  faEye,
+  faHeart,
+} from '@fortawesome/free-solid-svg-icons';
+
 import clsx from 'clsx';
 import { useDispatch } from 'react-redux';
 import {
   toggleFavoriteProduct,
   toggleCompareProduct,
 } from '../../../redux/productsRedux';
+import Button from '../Button/Button';
 import StarsRating from '../../features/StarsRating/StarsRating';
 
-const ProductBox = ({
+const ProductBox2 = ({
   id,
   name,
   price,
@@ -44,48 +49,57 @@ const ProductBox = ({
   return (
     <div className={styles.root}>
       <div className={styles.photo} style={{ backgroundImage: `url(${image})` }}>
-        {promo && <div className={styles.sale}>{promo}</div>}
-        <div className={styles.buttons}>
-          <Button variant='small'>Quick View</Button>
-          <Button variant='small'>
-            <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
-          </Button>
-        </div>
+        {/* {promo && <div className={styles.sale}>{promo}</div>} */}
       </div>
-      <div className={styles.content}>
-        <h5>{name}</h5>
-        <StarsRating stars={stars} userStars={userStars} id={id} />
-      </div>
-      <div className={styles.line}></div>
-      <div className={styles.actions}>
-        <div className={styles.outlines}>
-          <Button
-            variant='outline'
-            className={isFavoriteActive}
-            onClick={handleToggleFavorite}
-          >
-            <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
-          </Button>
-          <Button
-            variant='outline'
-            className={isCompareActive}
-            onClick={handleToggleCompare}
-          >
-            <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
-          </Button>
+      <div className={styles.overlay}>
+        <div className={styles.content}>
+          <h5>{name}</h5>
+          <StarsRating stars={stars} userStars={userStars} id={id} />
+
+          <div className={styles.price}>
+            <div className={styles.circleshadow}></div>
+            <Button noHover variant='small'>
+              ${price}.00
+            </Button>
+            {oldPrice ? <span className={styles.oldPrice}>${oldPrice}.00</span> : ''}
+          </div>
         </div>
-        <div className={styles.price}>
-          {oldPrice ? <span className={styles.oldPrice}>${oldPrice}</span> : ''}
-          <Button noHover variant='small'>
-            $ {price}
-          </Button>
+
+        <div className={styles.actions}>
+          <div className={styles.buttons}>
+            <Button
+              variant='outline'
+              className={clsx(styles.button, isFavoriteActive)}
+              onClick={handleToggleFavorite}
+            >
+              <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
+              <div className={styles.tooltip}>Favorite</div>
+            </Button>
+            <Button
+              variant='outline'
+              className={clsx(styles.button, isCompareActive, styles.compareButton)}
+              onClick={handleToggleCompare}
+            >
+              <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
+              <div className={styles.tooltip}>Compare</div>
+            </Button>
+
+            <Button variant='outline' className={styles.button}>
+              <FontAwesomeIcon icon={faEye}>Quick View</FontAwesomeIcon>
+              <div className={styles.tooltip}>Quick View</div>
+            </Button>
+            <Button variant='outline' className={styles.button}>
+              <FontAwesomeIcon icon={faShoppingBasket}>Add to Cart</FontAwesomeIcon>
+              <div className={styles.tooltip}>Add to Cart</div>
+            </Button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-ProductBox.propTypes = {
+ProductBox2.propTypes = {
   children: PropTypes.node,
   id: PropTypes.string,
   name: PropTypes.string,
@@ -99,4 +113,4 @@ ProductBox.propTypes = {
   oldPrice: PropTypes.number,
 };
 
-export default ProductBox;
+export default ProductBox2;
