46ef5a62d5915127729dd02671852f9593db804d
AS <arssoo569@gmail.com>
2024-06-01 21:19:25 +0200

    creating template of Promoted component

---
+++ src/components/features/Promoted/Promoted.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Promoted = () => {
+  return <div></div>;
+};
+
+export default Promoted;
--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -7,9 +7,11 @@ import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import CompareStickyBar from '../../features/CompareStickyBar/CompareStickyBar';
 import BrandsBar from '../../features/BrandsBar/BrandsBar';
+import Promoted from '../../features/Promoted/Promoted';
 
 const Homepage = () => (
   <div className={styles.root}>
+    <Promoted />
     <FeatureBoxes />
     <NewFurniture />
     <BrandsBar />
