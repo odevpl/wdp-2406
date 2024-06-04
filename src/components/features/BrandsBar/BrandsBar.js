6e819e99b92222a66cec889f1d54dcd32e2f8461
AS <arssoo569@gmail.com>
2024-06-04 10:54:26 +0200

    adding responsive functionalities

--- src/components/features/BrandsBar/BrandsBar.js
+++ src/components/features/BrandsBar/BrandsBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './BrandsBar.module.scss';
 import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,21 +6,67 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
 
 const BrandsBar = () => {
   const brands = useSelector(state => state.brands);
+  const [activeBrand, setActiveBrand] = useState(0);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [visibleBrands, setVisibleBrands] = useState([]);
+
+  const getViewportWidth = () =>
+    document.documentElement.clientWidth || window.innerWidth;
+
+  const numberOfVisibleBrands = wide =>
+    Math.min(brands.length, Math.max(1, Math.floor((wide - 220) / 150)));
+
+  const updateVisibleBrands = (startIndex, count) => {
+    const end = startIndex + count;
+    const brandsCount = brands.length;
+    let visible = [];
+    for (let i = startIndex; i < end; i++) {
+      visible.push(brands[i % brandsCount]);
+    }
+    setVisibleBrands(visible);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newWidth = getViewportWidth();
+      setWidth(newWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const count = numberOfVisibleBrands(width);
+    updateVisibleBrands(activeBrand, count);
+  }, [width, activeBrand, brands, numberOfVisibleBrands, updateVisibleBrands]);
+
+  const handleRightArrowClick = () => {
+    setActiveBrand(prevActiveBrand => (prevActiveBrand + 1) % brands.length);
+  };
+
+  const handleLeftArrowClick = () => {
+    setActiveBrand(
+      prevActiveBrand => (prevActiveBrand - 1 + brands.length) % brands.length
+    );
+  };
 
   return (
     <div className={styles.root}>
       <div className='container'>
         <div className={styles.panelBar}>
           <div className='row align-items-center justify-content-between'>
-            <button>
+            <button onClick={handleLeftArrowClick}>
               <FontAwesomeIcon icon={faChevronLeft} />
             </button>
-            {brands.map(brand => (
+            {visibleBrands.map(brand => (
               <div key={brand.id} className={styles.brandIcon}>
                 <img src={brand.image} alt={brand.name} />
               </div>
             ))}
-            <button>
+            <button onClick={handleRightArrowClick}>
               <FontAwesomeIcon icon={faChevronRight} />
             </button>
           </div>
